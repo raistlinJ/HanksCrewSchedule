ff0787da2a68c279eb95a8a27e6b7d75f1ada115
RaistlinJ <raistlinJ@users.noreply.github.com>
2026-08-28 13:15:45 -0600

    update test-prisma

--- rallly-source/packages/database/test-prisma.ts
+++ rallly-source/packages/database/test-prisma.ts
@@ -1,11 +1,14 @@
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-prisma.poll.findUnique({
-  where: { id: "WJOEm4M7E0JY" },
-  select: {
-    votes: {
-      where: { participant: { deleted: false } },
-      select: { type: true, participantId: true }
-    }
-  }
-}).then(res => console.log(JSON.stringify(res, null, 2))).finally(() => prisma.$disconnect());
+import { prisma } from "./src/client";
+
+prisma.poll
+  .findUnique({
+    where: { id: "WJOEm4M7E0JY" },
+    select: {
+      votes: {
+        where: { participant: { deleted: false } },
+        select: { type: true, participantId: true },
+      },
+    },
+  })
+  .then((result) => console.log(JSON.stringify(result, null, 2)))
+  .finally(() => prisma.$disconnect());
